282d1d1cd1f713b121a33d90a929967970613098
Kristijan Karanfilovski <kar.kristijan@gmail.com>
2022-03-17 23:09:54 +0100

    minor change

--- Homeworks/ExtraHomework/SEDC.ExtraHomework/SEDC.ExtraHomework.Task6/Program.cs
+++ Homeworks/ExtraHomework/SEDC.ExtraHomework/SEDC.ExtraHomework.Task6/Program.cs
@@ -9,6 +9,18 @@ namespace SEDC.ExtraHomework.Task6
             //Task 6:
             //Create a function to find the number of times a substring appears in a given string.
 
+            string name = "SEDC SEDCc SEDCcC SEDCcCc SEDCcCcC";
+
+            int result = Occurances(name, "c");
+
+            Console.WriteLine(result);
+        }
+
+        static int Occurances (string str, string substr)
+        {
+            int count = str.ToLower().Split(substr).Length - 1;
+
+            return count;
         }
     }
 }
